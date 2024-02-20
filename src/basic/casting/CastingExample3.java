ab4d56d58fe8206b1115536bc24739c586fc9071
jeonghwa <suejunghwa@gmail.com>
2024-02-20 17:19:22 +0900

    zava 기초 문법 ( 입/출력, data type)

---
+++ src/basic/casting/CastingExample3.java
@@ -0,0 +1,21 @@
+package basic.casting;
+
+public class CastingExample3 {
+    public static void main(String[] args) {
+
+        char c = 'B';
+        int i = 2;
+
+        int intResult = c + i;
+        char charResult = (char) (c+i);
+
+        System.out.println("intResult = " +intResult);
+        System.out.println("CharResult = " +charResult);
+
+        int k = 10;
+        double d = k / 4.0;
+        System.out.println("d = " + d);
+
+
+    }
+}
