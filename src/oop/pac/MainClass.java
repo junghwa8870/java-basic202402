4d4000262bea8cd1508e83fd6e691a830d2e1e10
jeonghwa <suejunghwa@gmail.com>
2024-02-23 17:39:12 +0900

    thissuper

---
+++ src/oop/pac/MainClass.java
@@ -0,0 +1,18 @@
+//패키지 선언은 항상 클래스 최 상단에 위치해야 합니다.
+package oop.pac;
+//작성중인 패키지와 다른 곳에 위치한 클래스 정보를 불러오려면 import가 필요합니다.
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        //다른 패키지 내의 같은 이름을 가진 클래스 객체를 생성할 때는
+        //반드시 패키지 경로를 직접 명시해 주셔야 합니다.
+        Apple a = new Apple();
+        oop.pac.juice.Apple = new Apple();
+
+        Banana b = new Banana();
+        Melon m = new Melon();
+
+        //java.util.Scanner sc = new java.util.Scanner(System.in);
+    }
+}
