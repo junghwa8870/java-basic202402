710c9689777471f24e6ec187ff7b45ae17c84e1b
jeonghwa <suejunghwa@gmail.com>
2024-03-06 17:14:36 +0900

    음악관리프로그램

--- src/etc/api/io/obj/SaveSnack.java
+++ src/etc/api/io/obj/SaveSnack.java
@@ -9,16 +9,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class SaveSnack {
+
     public static void main(String[] args) {
 
         List<Snack> snackList = new ArrayList<>();
         Collections.addAll(snackList,
-                new Snack("콘칲", 1970, 1500, "GOOD"),
-                new Snack("사브레", 1980, 2500, "BAD"),
-                new Snack("오징어집", 1985, 1800, "SOSO")
-                );
+                new Snack("콘칲", 1970, 1500, Snack.Taste.GOOD),
+                new Snack("사브레", 1980, 2500, Snack.Taste.BAD),
+                new Snack("오징어집", 1985, 1800, Snack.Taste.SOSO)
+        );
 
-        try(FileOutputStream fos = new FileOutputStream("C:/jeonghwaworkspace/snack.sav")) {
+        try(FileOutputStream fos = new FileOutputStream("C:/MyWorkspace/snack.sav")) {
 
             // 객체를 저장할 수 있는 보조 스트림
             ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -31,4 +32,17 @@ public class SaveSnack {
 
 
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/etc/api/io/obj/Snack.java
+++ src/etc/api/io/obj/Snack.java
@@ -1,17 +1,22 @@
 package etc.api.io.obj;
 
 import java.io.Serializable;
+
 // 객체를 스트림으로 전송하려면 직렬화가 가능해야 합니다.
-// 직렬화(serialize): 자바 언어에서 사용되는 Object 또는 Data를 다른 환경의 자바 프로그램에서 사용할 수 있도록
+// 직렬화(serialize): 자바 언어에서 사용되는 Object 또는 Data를 다른 환경의 자바 프로그램에서 사용 할 수 있도록
 // 바이트 형태의 연속적인 데이터로 변환하는 기술.
 public class Snack implements Serializable {
+
+    public enum Taste {
+        GOOD, BAD, SOSO
+    }
 
     private String snackName;
     private int year; // 출시연도
     private int price; // 가격
-    private String taste; // 맛
+    private Taste taste; // 맛
 
-    public Snack(String snackName, int year, int price, String taste) {
+    public Snack(String snackName, int year, int price, Taste taste) {
         this.snackName = snackName;
         this.year = year;
         this.price = price;
@@ -42,11 +47,11 @@ public class Snack implements Serializable {
         this.price = price;
     }
 
-    public String getTaste() {
+    public Taste getTaste() {
         return taste;
     }
 
-    public void setTaste(String taste) {
+    public void setTaste(Taste taste) {
         this.taste = taste;
     }
 
@@ -59,4 +64,13 @@ public class Snack implements Serializable {
                 ", taste='" + taste + '\'' +
                 '}';
     }
+
+
 }
+
+
+
+
+
+
+
